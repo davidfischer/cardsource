4326db37f900297f3f0ceae285d5216fbc3ec9f8
David Fischer <djfische@gmail.com>
2018-08-24 15:37:01 -0700

    Running a quick test

--- docs/conf.py
+++ docs/conf.py
@@ -93,7 +93,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -194,3 +194,6 @@ latex_documents = [
 
 # If false, no module index is generated.
 #latex_use_modindex = True
+
+def setup(app):
+    app.add_javascript('js/disable-search.js')
